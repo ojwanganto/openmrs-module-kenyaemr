ddbc2d9f139738fcdcfaf8a3b2bed6fba7c2c406
ojwanganto <ojwangwachiaje@gmail.com>
2023-01-30 12:43:18 +0300

    appointment notifier

--- api/src/main/java/org/openmrs/module/kenyaemr/appointmentNotifier/DefaultAppointmentCreationNotifier.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/appointmentNotifier/DefaultAppointmentCreationNotifier.java
@@ -11,11 +11,29 @@ package org.openmrs.module.kenyaemr.appointmentNotifier;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.Encounter;
+import org.openmrs.Form;
+import org.openmrs.Obs;
 import org.openmrs.Patient;
+import org.openmrs.api.EncounterService;
+import org.openmrs.api.FormService;
+import org.openmrs.api.VisitService;
+import org.openmrs.api.context.Context;
 import org.openmrs.module.appointments.model.Appointment;
+import org.openmrs.module.appointments.model.AppointmentServiceDefinition;
 import org.openmrs.module.appointments.notification.AppointmentEventNotifier;
 import org.openmrs.module.appointments.notification.NotificationException;
 import org.openmrs.module.appointments.notification.NotificationResult;
+import org.openmrs.module.appointments.service.AppointmentsService;
+import org.openmrs.module.kenyaemr.advice.SyncHFEAppointmentsWithBahmniModule;
+import org.openmrs.module.kenyaemr.metadata.HivMetadata;
+import org.openmrs.parameter.EncounterSearchCriteriaBuilder;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Set;
 
 /**
  * This class listens to events in the Bahmni appointments module
@@ -38,10 +56,17 @@ public class DefaultAppointmentCreationNotifier implements AppointmentEventNotif
 
     @Override
     public boolean isApplicable(final Appointment appointment) {
-        // just check if appointment is of type HIV follow up. We only want to limit this to appointment updates since creation is done in green card
-        if (appointment.getServiceType().equals("HIV follow up")) { // TODO: complete the check
+        AppointmentsService appointmentsService = Context.getService(AppointmentsService.class);
+        //AppointmentServiceDefinition definition = appoi
+        log.info("Appointment ID===============: " + appointment.getService().getName());
+        /**
+         * We don't want notification if it's a newly created Green card
+         */
+        if (appointment.getService().getUuid().equalsIgnoreCase(SyncHFEAppointmentsWithBahmniModule.HIV_FOLLOWUP_SERVICE)) {
+
             return true;
         }
+
         return false;
     }
 
@@ -49,8 +74,49 @@ public class DefaultAppointmentCreationNotifier implements AppointmentEventNotif
     public NotificationResult sendNotification(final Appointment appointment) throws NotificationException {
         // Check if the patient has an HIV followup visit on the appointment date. Align the date with that in the green card form
         Patient patient = appointment.getPatient();
-        log.info(appointment);
-        return new NotificationResult("", "EMAIL", 0, APPOINTMENT_SYNCHRONIZED);
+
+        VisitService visitService = Context.getVisitService();
+        EncounterService encounterService = Context.getEncounterService();
+        FormService formService = Context.getFormService();
+        Form hivRdeForm = formService.getFormByUuid(HivMetadata._Form.MOH_257_VISIT_SUMMARY);
+        Form hivGreencard = formService.getFormByUuid(HivMetadata._Form.HIV_GREEN_CARD);
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(appointment.getDateCreated());
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+
+        System.out.println("Appointment date: " + cal.getTime());
+
+        EncounterSearchCriteriaBuilder searchCriteriaBuilder = new EncounterSearchCriteriaBuilder();
+        searchCriteriaBuilder.setPatient(patient);
+        searchCriteriaBuilder.setFromDate(cal.getTime()); // we are checking for encounters created on same day as appointment
+        searchCriteriaBuilder.setEnteredViaForms(Arrays.asList(hivGreencard, hivRdeForm));
+        List<Encounter> hivFollowupEncounters = encounterService.getEncounters(searchCriteriaBuilder.createEncounterSearchCriteria());
+        Integer tcaConceptId = 5096;
+
+        // we want to pick just one encounter
+        Encounter followupEncounter = null;
+        if (hivFollowupEncounters.size() >= 1) {
+            followupEncounter = hivFollowupEncounters.get(0);
+        }
+
+        System.out.println("Encounter size:::::::::::::::;" + hivFollowupEncounters.size());
+        if (followupEncounter != null) {
+            Set<Obs> encounterObs = followupEncounter.getObs();
+            Obs appointmentObs = null;
+            for (Obs o : encounterObs) {
+                if (o.getConcept().getConceptId().equals(tcaConceptId)) {
+                    appointmentObs = o;
+                    break;
+                }
+            }
+            appointmentObs.setValueDatetime(appointment.getStartDateTime());
+            Context.getObsService().saveObs(appointmentObs, "Updated appointment date");
+        }
+        return new NotificationResult("", getMedium(), 0, APPOINTMENT_SYNCHRONIZED);
 
     }
 
